68bc02fd99189b42b761070e35a30c14ec947951
Jacinto Mendes <jmendes@greeningaustralia.org.au>
2021-06-12 17:33:28 +1000

    trying the .env file

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const app = require("express")();
 
 const PORT = process.env.PORT || 3000;
 
+require('dotenv').config()
+// store mongo password separately
+mongodb_password = process.env.MONGO_PW
+
+let mystring= "helo world " + mongodb_password;
+
 app.get("", (req, res) => {
-	res.send("Hello world");
+	res.send(mystring);
 });
 
 app.listen(PORT, () => {
